b42edb26c428be33fa55289be0de90caaa929630
Bhanmaurya6033 <Bhanmaurya6033@gmail.com>
2025-03-19 06:01:56 +0000

    updated..

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 <<<<<<< HEAD
 hello this fine contains deployment scrips
 Hello updating in master branch
-
- updating in dev branchadded new line to dev branch
->>>>>>> development
+                        
+hello adding this to git fetch
